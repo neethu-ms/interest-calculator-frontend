f7a848ee1feeffa845ed47c0c5c00ffe23e82324
neethu <msneethu2989@gmail.com>
2020-05-25 17:50:48 -0400

    Add ui and styles for simple interest calculator

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        
+        <form onSubmit={() => ""}>
+          <h1>Simple Interest Calculator</h1>
+          <table>
+            <tbody>
+              <tr>
+                <td>
+                  <label>Principal</label>
+                </td>
+                <td>
+                  <input type="text" />{" "}
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Rate of Interest</label>
+                </td>
+                <td>
+                  <input type="text" />
+                </td>
+              </tr>
+              <tr>
+                <td>
+                  <label>Number of Years</label>
+                </td>
+                <td>
+                  <input type="text" />
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <br />
+          <button>Calculate</button>
+        </form>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
